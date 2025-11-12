df7a3aa8394afbf5236a86ee3fcef81e22bc0041
jking7988 <75623121+jking7988@users.noreply.github.com>
2025-11-11 23:01:54 -0600

    a

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {structureTool} from 'sanity/structure'   // optional
 import {visionTool} from '@sanity/vision'
-import schemaTypes from './schemaTypes' // ← default export, not {schema}
+import schemaTypes from './schemaTypes'          // default export
 
 export default defineConfig({
   name: 'default',
@@ -9,8 +9,6 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   basePath: '/studio',
-  plugins: [structureTool(), visionTool()],
-  schema: {
-    types: schemaTypes, // ← pass the array of types
-  },
+  plugins: [structureTool(), visionTool()],      // or just [visionTool()]
+  schema: { types: schemaTypes },
 })
