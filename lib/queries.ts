ed30b985f409169b000410d9ed0dad7fe5ace348
jking7988 <75623121+jking7988@users.noreply.github.com>
2025-11-12 22:44:36 -0600

    Update queries.ts

--- lib/queries.ts
+++ lib/queries.ts
@@ -2,8 +2,15 @@ import { groq } from 'next-sanity';
 
 export const homeQuery = groq`{
   "posts": *[_type == "post"] | order(_createdAt desc)[0..5]{ _id, title, excerpt, body },
-  "events": *[_type == "event" && dateTime(start) >= now()] | order(start asc)[0..4]{ _id, title, start, end, location, description }
-}`;
+"events": *[_type == "event" && dateTime(coalesce(startDate, start)) >= now()] 
+  | order(coalesce(startDate, start) asc)[0..4]{
+    _id,
+    title,
+    "startDate": coalesce(startDate, start),
+    "endDate": coalesce(endDate, end),
+    location,
+    description
+  }`;
 
 export const postsQuery = groq`*[_type == "post"] | order(_createdAt desc) {
   _id,
@@ -13,13 +20,14 @@ export const postsQuery = groq`*[_type == "post"] | order(_createdAt desc) {
   _createdAt
 }`;
 
-export const eventsQuery = groq`*[_type == "event" && defined(startDate)] | order(startDate asc) {
-  _id,
-  title,
-  description,
-  location,
-  startDate,
-  endDate
-}`;
+export const eventsQuery = groq`*[_type == "event" && defined(coalesce(startDate, start))]
+  | order(coalesce(startDate, start) asc) {
+    _id,
+    title,
+    description,
+    location,
+    "startDate": coalesce(startDate, start),
+    "endDate": coalesce(endDate, end)
+  }`;
 
 export const documentsQuery = groq`*[_type == "documentFile"] | order(_createdAt desc){ _id, title, description, category, "fileUrl": file.asset->url }`;
